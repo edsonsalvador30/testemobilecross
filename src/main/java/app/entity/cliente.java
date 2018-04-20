f4612b37e745292a7fba5e7cb781273e0bb949c2
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-04-20 11:19:12 +0000

    Segundo Commit

---
+++ src/main/java/app/entity/cliente.java
@@ -0,0 +1,141 @@
+package app.entity;
+
+import java.io.*;
+import javax.persistence.*;
+import java.util.*;
+import javax.xml.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import cronapi.rest.security.CronappSecurity;
+
+
+/**
+ * Classe que representa a tabela CLIENTE
+ * @generated
+ */
+@Entity
+@Table(name = "\"CLIENTE\"")
+@XmlRootElement
+@CronappSecurity
+@JsonFilter("app.entity.cliente")
+public class cliente implements Serializable {
+
+  /**
+   * UID da classe, necessário na serialização
+   * @generated
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @generated
+   */
+  @Id
+  @Column(name = "id", nullable = false, insertable=true, updatable=true)
+  private java.lang.String id = UUID.randomUUID().toString().toUpperCase();
+
+  /**
+  * @generated
+  */
+  @Column(name = "nome", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.lang.String nome;
+
+  /**
+  * @generated
+  */
+  @OneToOne
+  @JoinColumn(name="fk_banco", nullable = true, referencedColumnName = "id", insertable=true, updatable=true)
+  
+  private Banco banco;
+
+  /**
+   * Construtor
+   * @generated
+   */
+  public cliente(){
+  }
+
+
+  /**
+   * Obtém id
+   * return id
+   * @generated
+   */
+  
+  public java.lang.String getId(){
+    return this.id;
+  }
+
+  /**
+   * Define id
+   * @param id id
+   * @generated
+   */
+  public cliente setId(java.lang.String id){
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Obtém nome
+   * return nome
+   * @generated
+   */
+  
+  public java.lang.String getNome(){
+    return this.nome;
+  }
+
+  /**
+   * Define nome
+   * @param nome nome
+   * @generated
+   */
+  public cliente setNome(java.lang.String nome){
+    this.nome = nome;
+    return this;
+  }
+
+  /**
+   * Obtém banco
+   * return banco
+   * @generated
+   */
+  
+  public Banco getBanco(){
+    return this.banco;
+  }
+
+  /**
+   * Define banco
+   * @param banco banco
+   * @generated
+   */
+  public cliente setBanco(Banco banco){
+    this.banco = banco;
+    return this;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    cliente object = (cliente)obj;
+    if (id != null ? !id.equals(object.id) : object.id != null) return false;
+    return true;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public int hashCode() {
+    int result = 1;
+    result = 31 * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+}
